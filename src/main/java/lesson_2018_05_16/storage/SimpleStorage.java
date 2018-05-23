0c98413506bf6b7b78e11ae728fc6e3150023fd7
Ilia Ilinykh <IlyasYOY@gmail.com>
2018-05-23 13:42:18 +0300

    Now done

--- src/main/java/lesson_2018_05_16/storage/SimpleStorage.java
+++ src/main/java/lesson_2018_05_16/storage/SimpleStorage.java
@@ -11,26 +11,20 @@ import java.util.concurrent.TimeUnit;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class SimpleStorage implements Storage {
     static final Object writerLock = new Object();
-    static final Object readerLock = new Object();
+    volatile int writersCount;
     volatile String string = "DEFAULT";
-    volatile boolean nowWriting;
 
     @Override
     @SneakyThrows
     public String getString() {
-        String result;
-        if (!nowWriting) {
-            TimeUnit.SECONDS.sleep(1);
-            result = string;
-        } else {
-            synchronized (writerLock) {
-                while (nowWriting) {
-                    writerLock.wait();
-                }
-                TimeUnit.SECONDS.sleep(1);
-                result = string;
+        synchronized (writerLock) {
+            while (writersCount > 0) {
+                writerLock.wait();
             }
         }
+
+        String result;
+        result = unsaveGet();
         return result;
     }
 
@@ -38,14 +32,27 @@ public class SimpleStorage implements Storage {
     @SneakyThrows
     public void setString(final String string) {
         synchronized (writerLock) {
-            nowWriting = true;
-            TimeUnit.SECONDS.sleep(1);
-            this.string = string;
-            if (nowWriting) {
-                nowWriting = false;
-                writerLock.notifyAll();
+            writersCount++;
+            while (writersCount > 1) {
+                writerLock.wait();
             }
         }
+        unsaveSet(string);
+        synchronized (writerLock) {
+            writersCount--;
+            writerLock.notifyAll();
+        }
+    }
+
+    @SneakyThrows
+    private String unsaveGet() {
+        TimeUnit.SECONDS.sleep(1);
+        return string;
+    }
+
+    @SneakyThrows
+    private void unsaveSet(final String string) {
+        TimeUnit.SECONDS.sleep(1);
+        this.string = string;
     }
 }
-
